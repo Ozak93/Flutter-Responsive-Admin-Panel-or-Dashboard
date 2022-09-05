8c15c7971d99c30a4c58348fbf2dde392ab3d55a
Osama Zakarneh <osamazakarneh@gmail.com>
2022-09-06 02:00:15 +0300

    test 3

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:admin/constants.dart';
 import 'package:admin/controllers/MenuController.dart';
 import 'package:admin/screens/main/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -12,11 +12,20 @@ import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  await Firebase.initializeApp(
+  var app = await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FirebaseMessagingWeb().getToken().then((value) => log("fcm token: ${value}"));
+  FirebaseMessaging.instance
+      .getToken(
+          vapidKey:
+              "BOkhbcBt40JbBf5mVkYLNa8c83w6F9YwU92o4cj1ekqWTkthmQ5CwEUxER1AvRMg7QwU6AeaniliWUPBoFSKA4Y")
+      .then((value) => log("fcm token: ${value}"));
+
+  FirebaseMessaging.onMessage.listen((event) {
+    log("caught fcm msg ${event.toString()}");
+  });
   log("firebase is ${Firebase.apps.first.toString()}");
+  log("hello 1");
   runApp(MyApp());
 }
 
---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,11 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+/*
+const app =FirebaseApp.intializeApp();
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((message) => {
+  console.log("onBackgroundMessage", message);
+});*/
